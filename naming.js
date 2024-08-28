6f8650da14a5f9cd55ad11499a257ec1a33cea1e
ashraful2871 <ashrafulislam2758@gmail.com>
2024-08-28 20:10:40 +0600

    add naming.js

---
+++ naming.js
@@ -0,0 +1,28 @@
+// 1. no keyword use in variable name
+var false =50: // wrong 
+var var =50: //wrong 
+
+// 2. no space or gape in variable name
+var is happy = false; //wrong
+var ishappy = true //correct
+
+// 3. no quote
+var "address" = "Kochu Khet"; 
+
+// can not start with a number. number other then the first letter is allowed
+var money =500;
+var money2 =500;
+var 3money =500;//wrong
+
+// 5. name is case sensitive
+address
+Address
+ADDress
+addRESS 
+
+// How to write a long variable name
+var my current home address = "Andar Killa Bandarban"
+var mycurrenthomeaddress = "Andar Killa Bandarban"
+var my_current_home_address = "Andar Killa Bandarban" //snake case
+var myCurrentHomeAddress = "Andar Killa Bandarban" //camel case
+var MyCurrentHomeAddress = "Andar Killa Bandarban" //pascal case
---
+++ types.js
@@ -0,0 +1,16 @@
+// string
+var name = "Ashraful Islam";
+var address = "Adar killa bandarban with some kataban";
+var country = "Bangladesh";
+
+// boolean
+var isRich = true;
+var passed = false;
+
+console.log(passed);
+console.log(typeof passed);
+console.log(typeof country);
+
+// number
+var price = 600;
+console.log(typeof price);
